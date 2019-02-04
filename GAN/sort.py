60293af8a3e9993d978ab084f6ce4a016808d6c3
Andrew <excisionhd1@gmail.com>
2019-02-03 21:04:44 -0800

    list practice

---
+++ GAN/sort.py
@@ -0,0 +1,9 @@
+list = [0.743, 0.840, 0.872, 0.908, 0.926, 0.931, 0.977, 1.008, 1.040, 1.047, 1.085, 1.115, 1.139, 1.151, 1.235, 1.360]
+
+list.sort()
+
+print(len(list))
+print(list)
+mid = list[len(list)/2]
+num = list[len(list)] - mid
+print(num)
